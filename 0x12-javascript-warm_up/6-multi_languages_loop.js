458c5917b686eb464af3a7e9a48782ca53690085
Ndivhuwo26 <mogakwekevin@gmail.com>
2024-06-19 01:40:51 -0700

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,4 +2,8 @@
 /* script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop */
 const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
-lines.forEach(line => console.log(line));
+let output = "";
+for (let i = 0; i < lines.length; i++) {
+  output += `${lines[i]}\n`;
+}
+console.log(output.trim());
