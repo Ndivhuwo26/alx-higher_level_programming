96e3ceb4c77053e118a6c7ed1e46506b19d68f9f
Ndivhuwo26 <mogakwekevin@gmail.com>
2024-06-19 01:50:13 -0700

    update

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 /* script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop */
-const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-let output = "";
+let output = '';
 for (let i = 0; i < lines.length; i++) {
   output += `${lines[i]}\n`;
 }
