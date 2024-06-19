d1a7459c81f7795f6f8b2834ebf8113730377c84
Ndivhuwo26 <mogakwekevin@gmail.com>
2024-06-19 02:00:22 -0700

    update

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,7 +5,9 @@ const x = parseInt(process.argv[2], 10);
 if (isNaN(x) || x <= 0) {
   console.log('Missing number of occurrences');
 } else {
+  let output = '';
   for (let i = 0; i < x; i++) {
-    console.log('C is fun');
+    output += 'C is fun\n';
   }
+  console.log(output.trim());
 }
