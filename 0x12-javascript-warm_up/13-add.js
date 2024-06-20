e9f9976c0d619fd2b43a7b2e8e6142ed39662c91
Ndivhuwo26 <mogakwekevin@gmail.com>
2024-06-20 00:54:43 -0700

    Add file

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,6 @@
+/* function that returns the addition of 2 integers */
+const add = (a, b) => {
+  return a + b;
+};
+
+module.exports = { add };
---
+++ 0x12-javascript-warm_up/13-main.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+/* function that returns the addition of 2 integers */
+const { add } = require('./13-add');
+
+console.log(add(3, 5));
