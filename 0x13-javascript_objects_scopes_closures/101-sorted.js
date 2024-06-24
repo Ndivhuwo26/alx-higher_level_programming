9766c45f1187724aaa60d6d8a6cbacb1c95de123
Ndivhuwo26 <mogakwekevin@gmail.com>
2024-06-24 01:10:08 -0700

    update

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* a script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence */
+exports.dict = {
+  89: 1,
+  90: 2,
+  91: 1,
+  92: 3,
+  93: 1,
+  94: 2
+};
