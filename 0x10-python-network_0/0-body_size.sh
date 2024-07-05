de46ebd6d7a97609053d8f50d485806f088c1707
Ndivhuwo26 <mogakwekevin@gmail.com>
2024-07-04 23:52:08 -0700

    update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # this a Bash script that will  takes in a URL, sends a request to that URL, and displays the size of the body of the response
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
 curl -s "$1" | wc -c
