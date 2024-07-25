8df33e529d9e1cb052922ab33645abeae3d5d0a0
Ndivhuwo26 <mogakwekevin@gmail.com>
2024-07-25 01:35:07 -0700

    update

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,6 @@
---this will List privileges for user_0d_1
-SHOW GRANTS FOR user_0d_1@localhost;
---this will List privileges for user_0d_2
-SHOW GRANTS FOR user_0d_2@localhost;
+-- This will list privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- This will list privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
