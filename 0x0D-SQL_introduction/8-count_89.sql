bba7e400bf23b04ea8a99f44bfc14854b9107812
Ndivhuwo26 <mogakwekevin@gmail.com>
2024-07-26 00:53:43 -0700

    updated

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,6 +1,3 @@
---cript that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server
-
-SELECT COUNT(*)
-FROM 'first_table'
-WHERE  'id' = 89 ;
-
+-- This will displays the number of records
+-- The  query to display the number of records with id = 89 in a table
+SELECT COUNT(*) as id FROM first_table WHERE id=89;
