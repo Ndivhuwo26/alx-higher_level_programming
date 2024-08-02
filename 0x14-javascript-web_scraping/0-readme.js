981a037ef1db64195199e40642331eeb0416ca8f
Ndivhuwo26 <mogakwekevin@gmail.com>
2024-08-02 00:13:48 -0700

    update

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
-//this script will read and print the content of a file.
-const filesys = require('fs');
-filesys.readFile(process.argv[2], 'utf-8',
-  function (err, data) {
-    if (err) {
-      console.log(err);
-      return;
-    }
+// this script will read and print the content of a file.
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
     console.log(data);
-  });
+  }
+});
