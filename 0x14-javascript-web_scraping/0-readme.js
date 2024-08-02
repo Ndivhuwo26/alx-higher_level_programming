ce92fd25420a0842aea7768413792562668e4fff
Ndivhuwo26 <mogakwekevin@gmail.com>
2024-08-01 23:57:04 -0700

    update

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
 //this script will read and print the content of a file.
-
-const fs = require('fs');
-
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-  } else {
+const filesys = require('fs');
+filesys.readFile(process.argv[2], 'utf-8',
+  function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
     console.log(data);
-  }
-});
-
+  });
