a452588796e36394d68a8eaf1fcf9c1972dbcdd5
Ndivhuwo26 <mogakwekevin@gmail.com>
2024-08-01 23:49:50 -0700

    update

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+//this will read from file
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
+
